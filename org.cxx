26dc1a0532dee751aaf2ada8a96eb199956899fb
Gadir <gadir.mamedov26@mail.ru>
2016-11-28 21:34:44 +0300

    spent 2h

--- org.cxx
+++ org.cxx
@@ -1,7 +1,18 @@
 #encoding "utf-8"
 
 OrgDescr -> AnyWord<kwtype="org_descr">;
+OrgHead -> AnyWord<kwtype="org_head">;
 
-Org -> OrgDescr interp(Object.Descr) AnyWord<h-reg1> interp(Object.Name; Object.Type="ORG");
+OrgDescr -> Adj <gnc-agr[1]> OrgDescr<rt,gnc-agr[1]> GP;
 
+GP -> Word<gram="S, gen">;
+
+Quoted_Words -> AnyWord <l-quoted, r-quoted>;
+Quoted_Words -> AnyWord<l-quoted, ~r-quoted>  Word<~l-quoted, ~r-quoted>* AnyWord <~l-quoted, r-quoted>;
+
+Org -> OrgDescr interp(Object.Descr) Word<h-reg1> interp(Object.Name; Object.Type="ORG");
+Org -> OrgDescr interp(Object.Descr) Quoted_Words interp(Object.Name; Object.Type="ORG");
+Org_noninterp -> OrgHead  AnyWord<h-reg1>;
+
+Org -> Org_noninterp interp (Object.Name; Object.Type ="ORG");
 Root -> Org;
